156847ab11c8f8260f321ca9fb078bf6f6298af6
d-lan1 <dylan.riley96@googlemail.com>
2024-09-02 17:26:39 +0100

    Added Data Transfer Objects (DTOs) to Models Project

---
+++ ShopOnlineCodeAlong.Modells/Dtos/CartIttemToAddDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnlineCodeAlong.Modells.Dtos
+{
+    public class CartIttemToAddDto
+    {
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Qty { get; set; }
+    }
+}
